3df3fedeb57dc396ca75001b910bdd98ad830035
Minwookim <trte135@naver.com>
2021-12-09 23:07:27 +0900

    2021.12.08 [JS - Day1]

---
+++ Day1.js
@@ -0,0 +1,19 @@
+/*
+    계산기 만들기
+*/
+
+function plus(a, b) {
+    console.log(a + b);
+};
+
+function minus(a, b) {
+    console.log(a - b);
+}
+
+function Mult(a, b) {
+    console.log(a * b);
+}
+
+function division(a, b) {
+    console.log(a / b);
+}
