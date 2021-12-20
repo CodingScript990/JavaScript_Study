43fae79e7e838a3d05158d9e8a3bbe95adae385a
Minwookim <trte135@naver.com>
2021-12-20 22:02:53 +0900

    2021.12.20 [JS - Day9] ✔ flow chart

---
+++ Day9.js
@@ -0,0 +1,23 @@
+// Flow Chart[순서도]
+// programming thinking ability[프로그래밍 사고력]
+// programming drawing chart[프로그래밍 순서도]
+
+// 절차, 예외, 버그를 잡아가는게 좋다(모든 가능성을 고려하여 절차를 구상한다.)
+// 범용적인 단어를 사용할 것!
+
+// 끝말잇기 게임 순서도
+
+/*
+    [Game start] -> [Choose how many people will participate?] (몇명에서 시작할 것인지 선택)
+                                ↓
+                    [Decide the order of participants] (참가자 시작 순서정하기)
+                                ↓
+                    [The first person says the keyword] (첫 번째 시작하는 참가자가 제시어를 말한다)
+                                ↓
+                    [The next person says a word] (다음 참가자가 대답한다)
+                        (Yes)↑    ↓(No)
+                    [Is the word correct?] (단어체크)   ->    [Mark that it's wrong] (틀렸다면?)
+                                                        (No)             ↓
+                                                                    [Game End] (게임오버)
+*/
+
